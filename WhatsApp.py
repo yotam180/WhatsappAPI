3a2f0ab32646c7e828c1317836279db23c58afb0
Yotam Salmon <yotam.salmon@gmail.com>
2017-11-12 12:50:09 +0200

    Reorganize WhatsAppDriver class

--- WhatsApp.py
+++ WhatsApp.py
@@ -2,8 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from json import loads, dumps
 
-def load_cert(cert_file, driver):
+"""
+The main class of the API to conenct to WhatsApp Web
+"""
+class WhatsAppDriver(object):
 
+	# Initiates a connection to WhatsApp Web
+	def __init__(self, cert_file, manual = True):
+		self.cert_file = cert_file
+		
+		self.driver = webdriver.Firefox()
+		driver = self.driver
+		driver.get("https://web.whatsapp.com")
+
+		if check_auth(driver) == False:
+			if (not load_cert("a.json", driver) or not check_auth(driver)) and manual:
+				while not check_auth(driver):
+					pass
+				ls = driver.execute_script("return JSON.stringify(localStorage);")
+				with open(cert_file, "w") as f:
+					f.write(ls)
+		
+		self.is_connected = check_auth(driver)
+		
+"""
+Loads a localStorage object from file and embeds it into a WhatsApp web. Then refreshes the web session
+"""
+def load_cert(cert_file, driver, refresh=True):
 	try:
 		with open(cert_file) as f:
 			localStorage = f.read()
@@ -14,41 +39,41 @@ def load_cert(cert_file, driver):
 	for k in ls.keys():
 		driver.execute_script("localStorage.setItem('" + k + "', " + dumps(ls[k]) + ");")
 		
-	driver.refresh()
+	if refresh:
+		driver.refresh()
 	
 	return True
-	
+
+"""
+Finds the barcode object in the page and returns its source (data:// url)
+If not present, returns None
+"""
 def find_barcode(driver):
 	try:
 		barcode = driver.find_element_by_css_selector("img[alt='Scan me!']")
 		return barcode.get_attribute("src")
 	except:
 		return None
-		
+
+"""
+Finds the barcode object in the page and returns its source (whatsapp cdn url)
+If not present, returns None
+"""		
 def find_avatar(driver):
 	try:
 		avatar = driver.find_element_by_class_name("avatar-image")
 		return avatar.get_attribute("src")
 	except:
 		return None
-	
+
+"""
+Determines if a WhatsApp Web page is authenticated or not.
+True - connected
+False - requires login
+"""
 def check_auth(driver):
 	avatar, barcode = None, None
 	while (avatar or barcode) == None:
 		avatar, barcode = find_avatar(driver), find_barcode(driver)
 		
 	return barcode == None
-
-def init(cert_file):
-	driver = webdriver.Firefox()
-	driver.get("https://web.whatsapp.com")
-
-	if check_auth(driver) == False:
-		if not load_cert("a.json", driver) or not check_auth(driver):
-			while not check_auth(driver):
-				pass
-			ls = driver.execute_script("return JSON.stringify(localStorage);")
-			with open(cert_file, "w") as f:
-				f.write(ls)
-	
-	return check_auth(driver), driver
