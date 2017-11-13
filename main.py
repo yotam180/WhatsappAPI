70080397dae18437f57e1d919cd8a266d55edf7e
Yotam Salmon <yotam.salmon@gmail.com>
2017-11-13 13:52:19 +0200

    Interface to Python (partial)

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from WhatsApp import WhatsAppDriver
+from selenium import webdriver
 
-whatsapp = WhatsAppDriver("a.json", manual=True)
+#opts = webdriver.ChromeOptions()
+#opts.add_argument("headless")
+#driver = webdriver.Chrome(chrome_options=opts)
+driver = webdriver.Chrome()
+whatsapp = WhatsAppDriver("a.json", manual=True, driver=driver, external=False)
 print whatsapp.is_connected
+print whatsapp.find_chats("f")
+whatsapp.close()
