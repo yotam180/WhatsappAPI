fb5fe0455a736a98bd11334a63f0c14257416dd6
Yotam Salmon <yotam.salmon@gmail.com>
2017-11-14 14:46:43 +0200

    Cancel 'External' parameter in Python API

--- py/main.py
+++ py/main.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 #opts.add_argument("headless")
 #driver = webdriver.Chrome(chrome_options=opts)
 driver = webdriver.Chrome()
-whatsapp = WhatsAppDriver("a.json", manual=True, driver=driver, external=False)
+whatsapp = WhatsAppDriver("a.json", manual=True, driver=driver)
 print whatsapp.is_connected
 print whatsapp.find_chats("f")
 whatsapp.close()
