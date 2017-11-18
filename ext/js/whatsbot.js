ef2d645001e5b92dcd33d600f150ece7614f9dbf
Yotam Salmon <yotam.salmon@gmail.com>
2017-11-18 14:48:54 +0200

    Create a control notification

--- ext/js/whatsbot.js
+++ ext/js/whatsbot.js
@@ -11,4 +11,18 @@
 	document.getElementById("whatsapp_messaging").addEventListener("content_message", function(e) {
 		messageFromServer(e.detail);
 	});
+	
+	function showControlMessage() {
+		m = document.createElement('div');
+		m.innerHTML = `<style>@-webkit-keyframes xopen { 0% {height: 0; width: 90%;} 100% {height: 5%; width: 100%;} } </style><div style="height: 5%; width: 100%; background-color: rgba(0, 204, 0, 0.4); z-index: 1000; position: fixed; text-align: center; vertical-align: middle; -webkit-animation: xopen 1s;"></div>`;
+		document.body.appendChild(m)
+		setTimeout(() => { m.children[1].innerHTML = "<br/>WhatsApp is being controlled by WhatsBot&trade;"; }, 1000)
+	}
+		
+	
+	
+	
+	API.ready().then(function() {
+		showControlMessage();
+	});
 })();
