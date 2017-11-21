8634cdfb661de2047a132db1bfe85291be8279db
Yotam Salmon <yotam.salmon@gmail.com>
2017-11-21 12:15:38 +0200

    WhatsBot now connects to local server

--- ext/js/whatsbot.js
+++ ext/js/whatsbot.js
@@ -4,8 +4,15 @@
 		document.getElementById("whatsapp_messaging").dispatchEvent(new CustomEvent("whatsapp_message", { "detail": data }));
 	};
 	
+	var handlers = {
+		"update_text": function(d) {
+			document.getElementById("text_msg").innerHTML = d.text;
+		}
+	};
+	
 	window.messageFromServer = function(data) {
 		console.log("Received message from server ", data);
+		handlers[data.type](data);
 	};
 	
 	document.getElementById("whatsapp_messaging").addEventListener("content_message", function(e) {
@@ -18,9 +25,9 @@
 		m.innerHTML = `<style>@-webkit-keyframes xopen { 0% {height: 90%; width: 100%;} 100% {height: 100%; width: 100%;} } </style><div style="height: 100%; width: 100%; background-color: rgba(0, 204, 0, 0.2); z-index: 1000; position: fixed; text-align: center; vertical-align: middle; -webkit-animation: xopen 1s;"></div>`;
 		document.body.appendChild(m)
 		setTimeout(() => { 
-			m.children[1].innerHTML = "<br/><br/><br/><br/><div style='margin: auto; width: 80%; padding: 30px; border-radius: 5px; background-color: white;'>WhatsApp is being controlled by WhatsBot&trade;<br/><br/><a id='cancel_bot' href='#'>Stop bot action</a></div>"; 
+			m.children[1].innerHTML = "<br/><br/><br/><br/><div style='margin: auto; width: 80%; padding: 30px; border-radius: 5px; background-color: white;'><div id='text_msg'>WhatsApp is being controlled by WhatsBot&trade;</div><br/><br/><a id='cancel_bot' href='#'>Stop bot action</a></div>"; 
 			document.getElementById("cancel_bot").addEventListener("click", stop_bot);
-		}, 1000)
+		}, 1);
 	}
 	
 	function removeControlMessage() {
