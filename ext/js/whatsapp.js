86de01460de9feb7d4f58ae32644f6ca2146d42d
Yotam Salmon <yotam.salmon@gmail.com>
2018-04-01 18:05:31 +0300

    Can now send  from Whatsapp web

--- ext/js/whatsapp.js
+++ ext/js/whatsapp.js
@@ -1,3 +1,5 @@
+window.ajaxes = {};
+
 /*
 Sends a message to the background page (content script -> background page [-> host])
 */
@@ -10,6 +12,15 @@ Receiving messages from the host/background page
 */
 window.messageFromServer = function(data) {
 	console.log("Received message from server ", data);
+	
+	if (window.ajaxes[data.ajax_id]) {
+		if (data.status == "success") {
+			window.ajaxes[data.ajax_id].success(data.e);
+		}
+		else {
+			window.ajaxes[data.ajax_id].error(data.a, data.b, data.c);
+		}
+	}
 };
 
 /*
@@ -18,3 +29,24 @@ For the DOM communication between the page and the content script
 document.getElementById("whatsapp_messaging").addEventListener("content_message", function(e) {
 	messageFromServer(e.detail);
 });
+
+window.$ajax = function(params) {
+	let success = null, error = null;
+	if (params.success) {
+		success = params.success;
+		params.success = null;
+	}
+	if (params.error) {
+		error = params.error;
+		params.error = null;
+	}
+	ajax_id = Math.random();
+	
+	window.ajaxes[ajax_id] = {success: success, error: error};
+	
+	serverMessage({
+		type: "ajax",
+		ajax_id: ajax_id,
+		ajax: params
+	});
+}
